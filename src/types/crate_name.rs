96568dea951acb332c026ae42e4132c2472ae5ae
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2024-12-30 18:25:35 +0700

    fix: update subtype

--- src/types/crate_name.rs
+++ src/types/crate_name.rs
@@ -1,7 +1,7 @@
-use subtype::newtype_string;
+use subtype::subtype_string;
 use subtype::{Empty, Not};
 
 // TODO: Add crate name validation
-newtype_string! {
+subtype_string! {
     pub struct CrateName(String | Not<Empty>)
 }
